af755fc51a8dd552900be5b89edfceea5edad942
erichanxx094-dev <erichanxx094@gmail.com>
2026-05-14 12:00:19 +0530

    Refactor Navbar component layout and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
 'use client'
-import Link from "next/link"
+
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-    <Link href="/">
-    Home
-    </Link>
+    <nav className="flex items-center justify-between px-6 py-4 border-b border-gray-800">
+      <Link href="/" className="text-xl font-bold text-white">
+        MissComic
+      </Link>
+      <ul className="flex gap-6 list-none">
+        <li>
+          <Link href="/browse" className="text-gray-400 hover:text-white transition">
+            Browse
+          </Link>
+        </li>
+        <li>
+          <Link href="/auth/login" className="text-gray-400 hover:text-white transition">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link href="/auth/signup" className="px-4 py-2 bg-purple-600 hover:bg-purple-700 rounded-lg text-white transition">
+            Sign up
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
+
 export default Navbar
