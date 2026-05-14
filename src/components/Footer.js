ff483905b28e625037af3744fff265e4c6c86211
erichanxx094-dev <erichanxx094@gmail.com>
2026-05-14 11:25:19 +0530

    Add Footer component with copyright notice

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center px-6 py-8 border-t border-gray-800">
+      <p className="text-gray-400 text-sm">
+        © 2024 MissComic. All rights reserved.
+      </p>
+    </footer>
+  )
+}
