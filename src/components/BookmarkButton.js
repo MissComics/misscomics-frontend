0c91077630500c9f210b5b123b3c7031fcc17836
erichanxx094-dev <erichanxx094@gmail.com>
2026-05-14 11:31:30 +0530

    Add BookmarkButton to ComicPage component

---
+++ src/components/BookmarkButton.js
@@ -0,0 +1,10 @@
+import BookmarkButton from '@/components/BookmarkButton'
+
+export default function ComicPage() {
+  return (
+    <div>
+      <h1>Comic Title</h1>
+      <BookmarkButton />
+    </div>
+  )
+}
